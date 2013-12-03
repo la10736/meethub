3b0098db735f3b95a2c61d19a91202a62b0f41dc
damico <damico@labsw-damico-laptop>
2013-12-03 14:08:23 +0100

    Aggiunto encoding

--- MeetHub/esperimenti/commissioni_imap.py
+++ MeetHub/esperimenti/commissioni_imap.py
@@ -8,22 +8,35 @@ import email
 import re
 import mysecret
 import logging
+import chardet
+from bs4 import BeautifulSoup as bs
 
 subject_re = re.compile(".*\s+Commissioni\s+Istruttorie\s+Zonali\s+-\s+(?P<azzione>.*\n*.*)Convocazione\s+Nr\s+(?P<id>\d+)\s+in\s+Data: (?P<data>\d{2}/\d{2}/\d{4})",re.MULTILINE)
 
-def get_first_text_block(self, email_message_instance):
-    maintype = email_message_instance.get_content_maintype()
-    if maintype == 'multipart':
-        for part in email_message_instance.get_payload():
-            if part.get_content_maintype() == 'text':
-                return part.get_payload()
-    elif maintype == 'text':
-        return email_message_instance.get_payload()
+def get_text(msg):
+    text = ""
+    if msg.is_multipart():
+        html = None
+        for part in msg.get_payload():
+            charset = part.get_content_charset()
+            if charset is None:
+                charset = chardet.detect(str(part))['encoding']
+            if part.get_content_type() == 'text/plain':
+                text = unicode(part.get_payload(decode=True),str(charset),"ignore").encode('utf8','replace')
+            if part.get_content_type() == 'text/html':
+                html = unicode(part.get_payload(decode=True),str(charset),"ignore").encode('utf8','replace')
+        if html is None:
+            return text.strip()
+        else:
+            return html.strip()
+    else:
+        text = unicode(msg.get_payload(decode=True),msg.get_content_charset(),'ignore').encode('utf8','replace')
+        return text.strip()
 
 cfg = {"address":"imap.gmail.com",
        "user":mysecret.user,
        "pwd":mysecret.pwd,
-       "folders":["inbox","M5S/Consiglio di Zona"]
+       "folders":["inbox"]#,"M5S/Consiglio di Zona"]
        }
 
 
@@ -55,6 +68,7 @@ for f in cfg["folders"]:
     for i in uid:
         rawm = mail.uid('fetch', i, '(RFC822)')[1][0][1]
         print "uid = " + i
+        print bs(get_text(email.message_from_string(rawm))).text
         #email_message = email.message_from_string(rawm)
         #print get_first_text_block(None,email_message)
 
